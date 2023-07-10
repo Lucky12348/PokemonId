dd311999b980c7e92127a03333cdb3cca15b92d1
sofiane <sofiane@sofiane.com>
2023-07-10 15:01:40 +0200

    first commit

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ from fastapi.templating import Jinja2Templates
 from typing import Optional, Any
 from pathlib import Path
 
-from app.schemas import RecipeSearchResults, Recipe, RecipeCreate
-from app.recipe_data import RECIPES
+"""
+mes from&import
+"""
 
+from app.schemas import Points
+from math import sqrt
+from typing import List
 
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
@@ -18,9 +22,6 @@ app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 api_router = APIRouter()
 
 
-# Updated to serve a Jinja2 template
-# https://www.starlette.io/templates/
-# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
 @api_router.get("/", status_code=200)
 def root(request: Request) -> dict:
     """
@@ -28,61 +29,8 @@ def root(request: Request) -> dict:
     """
     return TEMPLATES.TemplateResponse(
         "index.html",
-        {"request": request, "recipes": RECIPES},
+        {"request": request},
     )
-
-
-@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(*, recipe_id: int) -> Any:
-    """
-    Fetch a single recipe by ID
-    """
-
-    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
-    if not result:
-        # the exception is raised, not returned - you will get a validation
-        # error otherwise.
-        raise HTTPException(
-            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
-        )
-
-    return result[0]
-
-
-@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
-def search_recipes(
-    *,
-    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-    max_results: Optional[int] = 10,
-) -> dict:
-    """
-    Search for recipes based on label keyword
-    """
-    if not keyword:
-        # we use Python list slicing to limit results
-        # based on the max_results query parameter
-        return {"results": RECIPES[:max_results]}
-
-    results = filter(lambda recipe: keyword.lower() in recipe["label"].lower(), RECIPES)
-    return {"results": list(results)[:max_results]}
-
-
-@api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate) -> dict:
-    """
-    Create a new recipe (in memory only)
-    """
-    new_entry_id = len(RECIPES) + 1
-    recipe_entry = Recipe(
-        id=new_entry_id,
-        label=recipe_in.label,
-        source=recipe_in.source,
-        url=recipe_in.url,
-    )
-    RECIPES.append(recipe_entry.dict())
-
-    return recipe_entry
-
 
 # Nouvelle Route
 # Pokemon ID
@@ -95,9 +43,34 @@ def search_pokemon(request: Request, pokemon_id: int):
     data = r.json()
     pokemon_name = data["name"]
     pokemon_img_url = data["sprites"]["front_default"]
-    pokemon = {"name": pokemon_name, "image_url": pokemon_img_url}
-    return TEMPLATES.TemplateResponse("index.html", {"request": request, "recipes": RECIPES, "pokemon": pokemon})
 
+    # lister les types
+    pokemon_types = []
+    # A mettre a jour utiliser range & len pour data[types]
+    for i in range(len(data["types"])) :
+        pokemon_types.append(data["types"][i]["type"]["name"])
+    pokemon = {"name": pokemon_name, "image_url": pokemon_img_url,"type":pokemon_types}
+
+    return TEMPLATES.TemplateResponse("index.html", {"request": request, "pokemon": pokemon})
+
+
+
+@api_router.post("/compute_distance/",status_code=200)
+def compute_distance(points: List[Points] = [{"x": 0, "y": 0}, {"x": 0, "y": 0}]) -> dict: # defini un exemple
+    """
+    calculer les coordonnées
+    """
+
+    distance = 0.0
+
+    # recup la taille de la list
+    for i in range(len(points) - 1) : # i = 0 || ici on sait que seul 2 point sont utiliser
+        # mettre les valeurs dans 2 variables p1&p2
+        p1 = points[i]
+        p2 = points[i+1]
+    distance = sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
+
+    return {"resultat" : distance}
 
 
 
